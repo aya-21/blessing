a13a088fef122a308f3f5dcde883c1abe1c6d542
aya-21 <cucln825rf@gmail.com>
2023-08-06 16:19:49 +0900

    Add: イベント関連の処理を追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,38 @@
 class EventsController < ApplicationController
+  # GET /events/new
   def new
+    @event = Event.new
   end
 
+  # POST /events
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Event was successfully created.'
+    else
+      render :new
+    end
   end
 
+  # GET /events
   def index
+    @events = Event.all
   end
 
+  # GET /events/:id
   def show
+    @event = Event.find(params[:id])
+  end
+
+  # GET /events/:id/edit
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def event_params
+    params.require(:event).permit(:event_name, :date_at)
   end
 end
