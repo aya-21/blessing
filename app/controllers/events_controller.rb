041c42837cd2d89dfd7d657283f6e0986257bf1c
aya-21 <cucln825rf@gmail.com>
2023-08-18 22:21:52 +0900

    Add: イベント登録機能を追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,38 +1,32 @@
 class EventsController < ApplicationController
-  # GET /events/new
   def new
-    # @event = Event.new
+    @event = Event.new
   end
-
-  # POST /events
+  
   def create
-    # @event = Event.new(event_params)
-    # if @event.save
-    #   redirect_to @event, notice: 'Event was successfully created.'
-    # else
-    #   render :new
-    # end
-  end
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to new_event_path, notice: 'Event was successfully created.'
+    else
+      render :new
+    end
+  end 
 
-  # GET /events
   def index
-  #   @events = Event.all
+    @events = Event.all
   end
 
-  # GET /events/:id
   def show
-    # @event = Event.find(params[:id])
+    @event = Event.find(params[:id])
   end
 
-  # GET /events/:id/edit
   def edit
-    # @event = Event.find(params[:id])
+    @event = Event.find(params[:id])
   end
 
-  # private
+  private
 
-  # Only allow a list of trusted parameters through.
-  # def event_params
-  #   params.require(:event).permit(:event_name, :date_at)
-  # end
+  def event_params
+    params.require(:event).permit(:event_name, :date_at)
+  end
 end
