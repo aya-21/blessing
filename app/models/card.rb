c479dc6584dcd2300a24f46061cd0ef2692e084f
aya-21 <cucln825rf@gmail.com>
2023-08-30 13:46:36 +0900

    Add: イベントとカードの関連付けを行う

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,8 @@
 class Card < ApplicationRecord
+  mount_uploader :card_image, CardImageUploader
+
   belongs_to :user
+  belongs_to :event
 
   validates :card_name, presence: true
   validates :message, presence: true
